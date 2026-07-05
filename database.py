056085ffdb2fb74727a62762faf07fd74437e444
kishanrajpurohit260 <purohitkishan260@gmail.com>
2026-07-06 01:30:52 +0530

    Removed mediapipe

--- database.py
+++ database.py
@@ -1,6 +1,8 @@
 import os
 import mysql.connector
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 def connect_db():
     db = mysql.connector.connect(
         host=os.getenv("DB_HOST"),
@@ -8,6 +10,6 @@ def connect_db():
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME"),
         port=int(os.getenv("DB_PORT")),
-        ssl_ca="ca.pem"
+        ssl_ca=os.path.join(BASE_DIR, "ca.pem")
     )
     return db
