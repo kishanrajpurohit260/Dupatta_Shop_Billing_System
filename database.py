dbebd537cf080c9c51d05555886e2759580a24fa
kishanrajpurohit260 <purohitkishan260@gmail.com>
2026-07-06 11:00:00 +0530

    Show database error

--- database.py
+++ database.py
@@ -4,12 +4,18 @@ import mysql.connector
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 def connect_db():
-    db = mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME"),
-        port=int(os.getenv("DB_PORT")),
-        ssl_ca=os.path.join(BASE_DIR, "ca.pem")
-    )
-    return db
+    try:
+        db = mysql.connector.connect(
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME"),
+            port=int(os.getenv("DB_PORT")),
+            ssl_ca=os.path.join(BASE_DIR, "ca.pem")
+        )
+        print("✅ Database Connected")
+        return db
+
+    except Exception as e:
+        print("❌ DATABASE ERROR:", e)
+        raise
