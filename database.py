0f637bb5eb9c788467f03e6547b942f3bd39c0d7
kishanrajpurohit260 <purohitkishan260@gmail.com>
2026-07-06 11:29:07 +0530

    Support local and Render database connection

--- database.py
+++ database.py
@@ -4,8 +4,9 @@ import mysql.connector
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 def connect_db():
-    try:
-        db = mysql.connector.connect(
+
+    if os.getenv("DB_HOST"):   # Render/Aiven
+        return mysql.connector.connect(
             host=os.getenv("DB_HOST"),
             user=os.getenv("DB_USER"),
             password=os.getenv("DB_PASSWORD"),
@@ -13,9 +14,12 @@ def connect_db():
             port=int(os.getenv("DB_PORT")),
             ssl_ca=os.path.join(BASE_DIR, "ca.pem")
         )
-        print("✅ Database Connected")
-        return db
 
-    except Exception as e:
-        print("❌ DATABASE ERROR:", e)
-        raise
+    else:   # Local XAMPP
+        return mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="dupatta_shop",
+            port=3306
+        )
